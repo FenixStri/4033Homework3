54f95a9fd5b76ac27aa3672ca4d1a240526b2659
Fenix <89755740+FenixStri@users.noreply.github.com>
2022-10-04 13:00:08 -0500

    Update header.php

--- header.php
+++ header.php
@@ -14,22 +14,24 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="page1.html">Go To Page 1</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+        <li class="nav-item">
+          <a class="nav-link" href="page2.html">Go To Page 2</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="page3.html">Go To Page 3</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="page4.html">Go To Page 4</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="page5.html">Go To Page 5</a>
+        </li>
+       
         <li class="nav-item">
           <a class="nav-link disabled">Disabled</a>
         </li>
