21ef49d7a5b2b4123e1726b3a30507f27a95a110
Fenix <89755740+FenixStri@users.noreply.github.com>
2022-10-04 13:02:18 -0500

    Update header.php

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="page1.html">Go To Page 1</a>
